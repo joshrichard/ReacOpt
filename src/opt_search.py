bae2088f8add4151b57ee42f6cc862a410ad87e7
joshrichard <joshrichard@users.noreply.github.com>
2015-02-27 11:33:34 -0500

    tested the powdens calc component of peak fuel temp fit function

--- src/opt_search.py
+++ src/opt_search.py
@@ -118,7 +118,8 @@ def get_optim_opts(fit_dict, data_opts, fit_opts, case_info):
     vol_powdens = np.array([5.789E7, 7.815E7, 8.683E7, 1.172E8])[:,np.newaxis]
     peak_fuel_temp_regress = LinearRegression()
     peak_fuel_temp_regress.fit(vol_powdens, homog_peak_fuel_temps)
-    def calc_peak_bulk_fuel_temp(core_pow, core_height, powdens_calc = core.AssemblyPowerPeak(),
+    def calc_peak_bulk_fuel_temp(core_pow, core_height, 
+                                 powdens_calc = core.AssemblyPowerPeak(),
                                  regress_func=peak_fuel_temp_regress.predict):
         
         core_powdens = powdens_calc.set_core_conditions(core_pow, core_height)
@@ -185,7 +186,7 @@ def get_optim_opts(fit_dict, data_opts, fit_opts, case_info):
                                       - 1.0/(layer_k[idx]*layerrad[idx - 1]))
         t_max_constr_eval = t_max_constr - t_max
         return t_max_constr_eval
-        
+
 
 #    def constr_x1_upper(x):
 #        return 1 - x[0]
