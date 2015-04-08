156f7945287658c4cae6054f7ae316fd3909ee01
joshrichard <joshrichard@users.noreply.github.com>
2015-04-08 18:50:14 -0400

    Added file basename to plot_maker output files

--- utils/plot_maker.py
+++ utils/plot_maker.py
@@ -16,9 +16,10 @@ from scipy.spatial.distance import cdist, pdist, squareform, euclidean
 np.set_printoptions(precision=5, linewidth=90, suppress=True)
 
 data_dir = os.path.expanduser(os.path.join('~jgr42_000','Documents','Grad_Research','Salt_reactor',
-    'optimization_results','pow_iterations', 'lhs_110'))
-    
-table_filename = os.path.join(data_dir, 'locations.out')
+    'optimization_results','pow_iterations', 'nafzrf4', 'lhs_110'))
+
+maker_output_basename = 'nafzrf4_lhs_110_'
+table_filename = os.path.join(data_dir, maker_output_basename + 'locations.out')
     
 all_data_fname = os.path.join(data_dir, 'opt_run_dump_iter.out') # 'run_files', 
 #last_data_fname = os.path.join(data_dir, 'run_files', 'opt_run_dump_iter.out')
@@ -35,11 +36,11 @@ def main():
     # Plot expected improvement
     exp_imp = run_dump_data_list[-1]['all_search_res']
     exp_imp_axis = {'y_axis':'Expected Improvement'}
-    iter_val_plot(exp_imp, exp_imp_axis, 'lhs_110_exp_imp')
+    iter_val_plot(exp_imp, exp_imp_axis, 'exp_imp')
     # Plot optimal flux value
     optimal_flux = run_dump_data_list[-1]['all_opt_res']
     opt_flux_axis = {'y_axis':'Optimal Flux value [n/s/cm^2]'}
-    iter_val_plot(optimal_flux, opt_flux_axis, 'lhs_110_opt_val')
+    iter_val_plot(optimal_flux, opt_flux_axis, 'opt_val')
     ########################################
     # Print and plot the DOE eval locations:
     ########################################
@@ -49,15 +50,15 @@ def main():
     ref_loc_arr = np.zeros([1,doe_scaled.shape[1]])
     search_loc_dists = cdist(doe_scaled, ref_loc_arr)
     search_dist_axis = {'y_axis':'Distance from each search point to 0.0 vec'}
-    iter_val_plot(search_loc_dists, search_dist_axis, 'lhs_110_search_dist')
+    iter_val_plot(search_loc_dists, search_dist_axis, 'search_dist')
     # Plot Search location in relation to next location
     search_loc_rel_dists = calc_rel_dist(doe_scaled)
     search_rel_dist_axis = {'y_axis':'Distance from each search point to next'}
-    iter_val_plot(search_loc_rel_dists, search_rel_dist_axis, 'lhs_110_search_rel_dist')
+    iter_val_plot(search_loc_rel_dists, search_rel_dist_axis, 'search_rel_dist')
     # Plot dists from each search location to original search location
     search_loc_orig_dists = cdist(doe_scaled, doe_scaled[0, np.newaxis])[:,0]
     search_orig_dist_axis = {'y_axis':'Distance from each opt point to orig'}
-    iter_val_plot(search_loc_orig_dists, search_orig_dist_axis, 'lhs_110_opt_orig_dist')    
+    iter_val_plot(search_loc_orig_dists, search_orig_dist_axis, 'opt_orig_dist')    
     # Write out table of search point locations
     with open(table_filename, 'w') as f:
         f.write('DoE locations:' + os.linesep)
@@ -74,20 +75,20 @@ def main():
     # Plot dists from optimal location to zero point
     opt_loc_dists = cdist(opt_val_loc, ref_loc_arr)
     opt_dist_axis = {'y_axis':'Distance from each optimal location to 0.0 vec'}
-    iter_val_plot(opt_loc_dists, opt_dist_axis, 'lhs_110_opt_dist')
+    iter_val_plot(opt_loc_dists, opt_dist_axis, 'opt_dist')
     # Plot dists from optimal val location to next optimal val location
     opt_loc_rel_dists = calc_rel_dist(opt_val_loc)
     opt_rel_dist_axis = {'y_axis':'Distance from each optimal loc to next'}
-    iter_val_plot(opt_loc_rel_dists, opt_rel_dist_axis, 'lhs_110_opt_rel_dist')
+    iter_val_plot(opt_loc_rel_dists, opt_rel_dist_axis, 'opt_rel_dist')
     # Plot dists from each optimal val location to original opt val location
     opt_loc_orig_dists = cdist(opt_val_loc, opt_val_loc[0, np.newaxis])[:,0]
     opt_orig_dist_axis = {'y_axis':'Distance from each opt point to orig'}
-    iter_val_plot(opt_loc_orig_dists, opt_orig_dist_axis, 'lhs_110_opt_orig_dist')
+    iter_val_plot(opt_loc_orig_dists, opt_orig_dist_axis, 'opt_orig_dist')
     # plots dists from optimal val location to search point
     opt_search_dists = cdist(opt_val_loc, doe_scaled)
     opt_search_dists = np.diagonal(opt_search_dists).copy()
     opt_search_axis = {'y_axis':'Distance from optimum to search location'}
-    iter_val_plot(opt_search_dists, opt_search_axis, 'lhs_110_opt_search_dist')
+    iter_val_plot(opt_search_dists, opt_search_axis, 'opt_search_dist')
     # Write out table of optimal value locations
     with open(table_filename, 'a') as f:
         f.write(os.linesep)
@@ -115,7 +116,8 @@ def main():
     
     
 
-def iter_val_plot(all_iter_val, y_label, figname, major_mult=5, minor_mult=1, directory=data_dir):
+def iter_val_plot(all_iter_val, y_label, figname, major_mult=5, minor_mult=1,
+                  base_filename=maker_output_basename, directory=data_dir):
     num_iters = len(all_iter_val)
     x_val = np.arange(1, num_iters+1)
     axis_labels = {'x_axis':'Iteration #'}
@@ -133,7 +135,7 @@ def iter_val_plot(all_iter_val, y_label, figname, major_mult=5, minor_mult=1, di
     ax.xaxis.set_minor_locator(minor_locator)
     plt.xlabel(axis_labels['x_axis'])
     plt.ylabel(axis_labels['y_axis'])
-    figname = figname + '.png'
+    figname = base_filename + figname + '.png'
     plt.savefig(os.path.join(directory, figname), dpi=600.0)
     plt.close()
     
