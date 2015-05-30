1c987881824f8b70ecc16a6b201530a3f9b5941c
joshrichard <joshrichard@users.noreply.github.com>
2015-05-30 18:57:37 -0400

    fixed bug (missing pin) in pin peaking for smallpins_p1 to coolant_calc

--- utils/coolant_calc.py
+++ utils/coolant_calc.py
@@ -42,7 +42,7 @@ def main():
     
     # Serpent pin peaking values
     #pin_peak_vals = np.array([1.31,1.26,1.23,1.16,1.14,1.10,1.05,1.03]) smallpins_reg
-    pin_peak_vals = pin_peak_vals = np.array([1.69,1.61,1.47,1.39,1.30,1.27,1.17,1.10,1.07,1.01])
+    pin_peak_vals = pin_peak_vals = np.array([1.69,1.61,1.47,1.39,1.30,1.27,1.17,1.10,1.07,1.01,0.958])
     ax_peak_val = 1.2859
     rad_peak_val = 1.196
     
