1f928ee65ccb214663620fd4404e1f142eefd9b5
Joshua Richard <jgr426@gmail.com>
2015-07-22 20:28:45 -0400

    Added additional xs mod capability

--- utils/mod_files.py
+++ utils/mod_files.py
@@ -4,16 +4,21 @@ import re
 import subprocess
 #import pdb
 
-#direc = '/Volumes/HDD_250GB/Documents/grad_research/salt_reactor/inputs/smallpins/final_design/final_core_analysis_files/rods_in/cold_zero_power'
-direc = os.getcwd()
-serp_token = 'fhtr'
+direc = '/Volumes/HDD_250GB/Documents/grad_research/salt_reactor/inputs/smallpins/final_design/final_core_analysis_files/rods_in/cold_zero_power'
+#direc = os.getcwd()
 
 clean = 'off'
-xs_temp = 'on'
-ex_serp = 'on'
+xs_temp = 'off'
+ex_serp = 'off'
 undo_backup = 'off'
+get_keff = 'on'
 
+serp_token = 'fhtr'
+pdist_levels = 4
 
+new_xs_temp = '724.0' # K (724K = 450 C)
+new_xs_ext = '06c' # '03c'
+new_sab_ext = '16t' # '00t'
 
 def run_serpent(fname, root_dir):
     orig_dir = os.getcwd()
@@ -40,19 +45,23 @@ def adj_xs_temp(fname, root_dir):
             match_obj = re.search('([0-9]+)\.([0-9]+)c', line)
             if match_obj:
                 # on a cross section line
-                new_xs_val = '{}.03c'.format(match_obj.group(1))
+                new_xs_val = '{}.{}'.format(match_obj.group(1), new_xs_ext)
                 new_line = line.replace(match_obj.group(), new_xs_val)
                 f_string += new_line
             elif 'gre7' in line:
                 old_sab = line.split()[-1]
                 old_sab_ext = old_sab.split('.')[-1]
-                new_sab_ext = '00t'
                 new_line = line.replace(old_sab_ext, new_sab_ext)
                 f_string += new_line
             elif 'pbed' in line:
                 old_pdistpath = line.split()[-1]
-                new_pdistpath = '"../' + old_pdistpath[1:]
+                pdist_fname = os.path.basename(old_pdistpath)
+                new_pdistpath = '"' + r'../'*pdist_levels + old_pdistpath[1:] + '"'
                 new_line = line.replace(old_pdistpath, new_pdistpath)
+                f_string += new_line
+            elif 'tmp' in line and 'burn' in line:
+                tmp_match = re.search('(tmp)\s+([0-9]+\.[0-9]+)', line)
+                new_line = line.replace(tmp_match.group(2), new_xs_temp)
                 f_string += new_line
             elif 'Detector Specification' in line:
                 f_string += line
@@ -96,6 +105,20 @@ def clean_filename(fname, root_dir):
         print "Renamed {} to {}".format(fname, stripname+ext)
 
 
+def extract_keff(fname, root_dir, first_bu=True):
+    full_fname = os.path.join(root_dir, fname+'_res.m')
+    with open(full_fname, 'rb') as f:
+        for line in f:
+            if 'COL_KEFF' in line:
+                line_items = line.split()
+                keff = float(line_items[6])
+                print '{}'.format(root_dir)
+                print '{:.5e}'.format(keff)
+                if first_bu:
+                    break
+    return keff
+
+
 def main():
     for root, dirs, files in os.walk(direc):
         for file_name in files:
@@ -106,6 +129,8 @@ def main():
                     adj_xs_temp(file_name, root)
                 if ex_serp == 'on':
                     run_serpent(file_name, root)
+                if get_keff == 'on':
+                    extract_keff(file_name, root)
             if undo_backup == 'on' and '.backup' in file_name:
                 restore_backup(file_name, root)
 
